1d149eb590bc732adfe6544b8368f820f2c3d828
0TheAshenOne0 <oaojassim@gmail.com>
2026-02-13 15:07:46 +0000

    fixed bugs

--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,55 @@
-import java.awt.image.AbstractMultiResolutionImage;
-import java.util.Random;
-import java.util.Scanner;
-
-public class Main
-{
-
-public static void main(String[] args)
-{
-   Scanner inp = new Scanner(System.in);
+void main() {
+    Scanner inp = new Scanner(System.in);
     boolean keepPlaying = true;
     Random randomNum = new Random();
-    int comp = randomNum.nextInt(3);
+    int comp;
     String[] choices = {"rock", "paper", "scissors"};
-    String compChoice = "";
+    String compChoice;
 
 
+    while (keepPlaying) {
 
 
-        while (keepPlaying == true) {
+        comp = randomNum.nextInt(3);
+        compChoice = "";
+        IO.println("Rock, paper, scissors!");
+        String ans = inp.nextLine().toLowerCase();
+        switch (comp) {
+            case 0:
+                compChoice = choices[0];
+                break;
+            case 1:
+                compChoice = choices[1];
+                break;
+            case 2:
+                compChoice = choices[2];
 
 
-            comp = randomNum.nextInt(3);
-            compChoice = "";
-            System.out.println("Rock, paper, scissors!");
-             String ans = inp.nextLine().toLowerCase();
-            switch (comp)
-            {
-                case 0:
-                    compChoice = choices[0];
-                    break;
-                case 1:
-                    compChoice = choices[1];
-                    break;
-                case 2:
-                    compChoice = choices[2];
-
-
-            }
-
-            while(!ans.equals(choices[0]) && !ans.equals(choices[1]) && !ans.equals(choices[2])) {
-                System.out.println("Invalid answer! Pick rock, paper or scissors!");
-                ans = inp.nextLine().toLowerCase();
-            }
-
-
-            if (ans.equals(choices[0]) && compChoice.equals(choices[1])) {
-                System.out.println("Player: " + ans + " Computer: " + compChoice + ". Computer wins!");
-            } else if (ans.equals(choices[0]) && compChoice.equals(choices[2])) {
-                System.out.println("Player: " + ans + " Computer: " + compChoice + ". You win!");
-            } else if (ans.equals(choices[1]) && compChoice.equals(choices[2])) {
-                System.out.println("Player: " + ans + " Computer: " + compChoice + ". Computer wins!");
-            } else if (ans.equals(choices[2]) && compChoice.equals(choices[1])) {
-                System.out.println("Player: " + ans + " Computer: " + compChoice + ". You win!");
-            } else if (ans.equals(choices[2]) && compChoice.equals(choices[0])) {
-                System.out.println("Player: " + ans + " Computer: " + compChoice + ". Computer wins!");
-            } else if (ans.equals(choices[1]) && compChoice.equals(choices[0])) {
-                System.out.println("Player: " + ans + " Computer: " + compChoice + ". You win!");
-            } else {
-                System.out.println("Player:" + ans + "Computer: " + compChoice + ". Draw!");
-            }
-            System.out.println("Keep playing? Y/N");
-            ans = inp.nextLine();
-            if (ans.equals("y")) {
-                keepPlaying = true;
-            } else {
-                keepPlaying = false;
-            }
         }
+
+        while (!ans.equals(choices[0]) && !ans.equals(choices[1]) && !ans.equals(choices[2])) {
+            IO.println("Invalid answer! Pick rock, paper or scissors!");
+            ans = inp.nextLine().toLowerCase();
+        }
+
+
+        if (ans.equals(choices[0]) && compChoice.equals(choices[1])) {
+            IO.println("Player: " + ans + " Computer: " + compChoice + ". Computer wins!");
+        } else if (ans.equals(choices[0]) && compChoice.equals(choices[2])) {
+            IO.println("Player: " + ans + " Computer: " + compChoice + ". You win!");
+        } else if (ans.equals(choices[1]) && compChoice.equals(choices[2])) {
+            IO.println("Player: " + ans + " Computer: " + compChoice + ". Computer wins!");
+        } else if (ans.equals(choices[2]) && compChoice.equals(choices[1])) {
+            IO.println("Player: " + ans + " Computer: " + compChoice + ". You win!");
+        } else if (ans.equals(choices[2]) && compChoice.equals(choices[0])) {
+            IO.println("Player: " + ans + " Computer: " + compChoice + ". Computer wins!");
+        } else if (ans.equals(choices[1]) && compChoice.equals(choices[0])) {
+            IO.println("Player: " + ans + " Computer: " + compChoice + ". You win!");
+        } else {
+            IO.println("Player: " + ans + "Computer: " + compChoice + ". Draw!");
+        }
+        IO.println("Keep playing? Y/N");
+        ans = inp.nextLine().toLowerCase();
+        keepPlaying = !ans.equals("n");
     }
 }
